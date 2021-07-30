8a816ea89838594a975f3a012540941c00574dd7
rimji <88181976+rimji@users.noreply.github.com>
2021-07-30 14:21:02 +0530

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -3,13 +3,8 @@
 @section('content')
     <h1>Welcome to Rimji</h1>
 
-    <p>Buy at Amazon</p>
+    <p>Buy our products at Amazon</p>
     
     <p>Contact us: customercare@rimji.com</p>
-
-    <p>The HTML, CSS and JavaScript in this template are extremely minimal, which makes the code easy to build on top of or replace completely.</p>
-
-    <a href="https://ko-fi.com/Y8Y07ACJ" target="_blank" rel="noopener">
-        <img src="{{ media('kofi-button.png') }}" alt="ko-fi" width="223" height="30">
-    </a>
+    
 @endsection
