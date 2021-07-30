e05b1f343b152e53cc889204b0d6f1fee5373989
rimji <88181976+rimji@users.noreply.github.com>
2021-07-30 14:23:07 +0530

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,10 +1,8 @@
 @extends('_layouts.master')
 
 @section('content')
-    <h1>Welcome to Rimji</h1>
-
+    <center>
     <p>Buy our products at Amazon</p>
-    
     <p>Contact us: customercare@rimji.com</p>
-    
+    <center>
 @endsection
