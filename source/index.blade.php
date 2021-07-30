5d202e4d2693255c37ac124ddc9a15d5e42e2e12
rimji <88181976+rimji@users.noreply.github.com>
2021-07-30 17:15:02 +0530

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <center>
-    <p><img src="https://user-images.githubusercontent.com/88181976/127647479-d1434cbf-fe29-46bb-9c17-28ec743ac00f.jpg" alt="HTML" width="300" height="200"></p>
+    <p><img src="https://user-images.githubusercontent.com/88181976/127647479-d1434cbf-fe29-46bb-9c17-28ec743ac00f.jpg" alt="HTML" width="640" height="427"></p>
     
     <p>Buy on Amazon</p>
     </center>
