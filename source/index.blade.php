0f153479d96df0ad1633421ca8fb806e0d2c0a68
rimji <88181976+rimji@users.noreply.github.com>
2021-07-30 17:08:36 +0530

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -4,6 +4,8 @@
     <center>
     <p><img src="https://user-images.githubusercontent.com/88181976/127635613-e006d804-28df-4999-961c-e9f748c9f71e.png" alt="HTML" width="64" height="64"></p>
     
+    <p><img src="https://user-images.githubusercontent.com/88181976/127647479-d1434cbf-fe29-46bb-9c17-28ec743ac00f.jpg" alt="HTML" width="300" height="200"></p>
+    
     <p>Buy our products on Amazon</p>
     </center>
 @endsection
