20aec72ab3962474e3501bfe9f61f687103e047d
rimji <88181976+rimji@users.noreply.github.com>
2021-07-30 18:24:10 +0530

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -3,6 +3,6 @@
 @section('content')
     <center>
     <p><img src="https://user-images.githubusercontent.com/88181976/127647479-d1434cbf-fe29-46bb-9c17-28ec743ac00f.jpg" alt="HTML" width="640" height="427"></p>
-    <small><center><p>About            Contact</p></small>
+    <small><center><p>About  &nbsp Contact</p></small>
     </center>
 @endsection
