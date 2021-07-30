4d9362804362ff84b3b5e0e56e7731d4e1227897
rimji <88181976+rimji@users.noreply.github.com>
2021-07-30 19:40:36 +0530

    Update about.blade.php

--- source/about.blade.php
+++ source/about.blade.php
@@ -3,14 +3,7 @@
 @section('title', 'About')
 
 @section('content')
-    <h1>About</h1>
-
-    <p>We are {{ $page->owner->name }}</p>
-
-    <h2>Links:</h2>
-
-    <ul>
-        <li><a href="https://instagram.com/{{ $page->owner->twitter }}" target="_blank">Twitter</a></li>
-        <li><a href="https://facebook.com/{{ $page->owner->github }}" target="_blank">GitHub</a></li>
-    </ul>
+   <small><center>
+   We love and sell scrunchies !
+   </center> </small>
 @endsection
