f15cc25adfc38ab6bea0857fc10626a02212d6aa
rimji <88181976+rimji@users.noreply.github.com>
2021-07-30 15:31:27 +0530

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     <center>
     <img src="https://user-images.githubusercontent.com/88181976/127635613-e006d804-28df-4999-961c-e9f748c9f71e.png" alt="HTML" width="64" height="64">
+    
     <p>Buy our products on Amazon</p>
     </center>
 @endsection
