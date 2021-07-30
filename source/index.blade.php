7ce078505109f651c9a6d85c20edc3be03004540
rimji <88181976+rimji@users.noreply.github.com>
2021-07-30 13:07:49 +0530

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <h1>Welcome to Rimji</h1>
 
-    <p>Artisan Static is a starter template for building a static Jigsaw blog hosted on Netlify.</p>
+    <p>Rimji is a starter template for building a static Jigsaw blog hosted on Netlify.</p>
 
     <p>This comes with code highlighting, share buttons, comments, analytics, an RSS feed, a contact form, a CMS and more.</p>
 
