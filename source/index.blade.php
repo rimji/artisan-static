cd68457fc159346e9fe4fa4ce93e7aff6db507dd
rimji <88181976+rimji@users.noreply.github.com>
2021-08-06 16:12:37 +0530

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -2,7 +2,6 @@
 
 @section('content')
     <center>
-    <p></p>
     <small><center><a href="https://www.rimji.com/about/">About</a> &nbsp &nbsp <a href="https://www.rimji.com/contact/">Contact</a></small>
     </center>
 @endsection
