2dd91b27e0126e3479a2963d19112b37252dfba4
rimji <88181976+rimji@users.noreply.github.com>
2021-08-14 17:31:34 +0530

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -2,6 +2,10 @@
 
 @section('content')
     <center>
+    <img src="https://user-images.githubusercontent.com/88181976/129445546-3e5e85ce-4a99-495b-87b7-57762da1d841.jpg" alt="London" width="100" height="52"><br>
+    <img src="https://user-images.githubusercontent.com/88181976/129445551-af4051c2-5c77-4cae-97e8-aad1fb456406.jpg" alt="Paris" width="100" height="52"><br>
+    <img src="https://user-images.githubusercontent.com/88181976/129445553-82c66d01-3f04-41ce-99a3-bb0e0370e230.jpg" alt="New York" width="100" height="52"><br>
+    <img src="https://user-images.githubusercontent.com/88181976/129445556-b8d2b1c8-29ea-4a96-970b-f83a05162e05.jpg" alt="Tokyo" width="100" height="52"><br>
     <small><center>London</a> &nbsp &nbsp Paris &nbsp &nbsp New York</a> &nbsp &nbsp Tokyo</a></small>
     </center>
 @endsection
