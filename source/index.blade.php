0220321868ef81bc3a4d666c1efd88e7d577939d
rimji <88181976+rimji@users.noreply.github.com>
2021-08-19 17:14:49 +0530

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -2,7 +2,6 @@
 
 @section('content')
     <center>
-    <img src="https://user-images.githubusercontent.com/88181976/130062429-28a0ed84-381a-4ade-aa19-26a3b4f17c3e.png" alt="Rimji" width="50" height="50">
     <small><center>London</a> &nbsp &nbsp Paris &nbsp &nbsp New York</a> &nbsp &nbsp Tokyo</a></small>
     </center>
 @endsection
