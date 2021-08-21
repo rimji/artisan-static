427880e9aa28192821b8d31e58b0c422b8156adf
rimji <88181976+rimji@users.noreply.github.com>
2021-08-21 19:40:40 +0530

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -2,6 +2,8 @@
 
 @section('content')
     <center>
-    <small><center>London</a> &nbsp &nbsp Paris &nbsp &nbsp New York</a> &nbsp &nbsp Tokyo</a></small>
+    <small> 
+
+    </small>
     </center>
 @endsection
