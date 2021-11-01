a3cb2b796991712319fbaf27f99d7d73cd9b9cd8
rimji <88181976+rimji@users.noreply.github.com>
2021-11-01 10:32:52 +0530

    Update master.blade.php

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -23,19 +23,6 @@
             <strong><img src="https://user-images.githubusercontent.com/88181976/139623744-b8ae9ed8-3824-4e89-85ea-e2c60a197d3a.png" alt="Rimji" width="200" height="500"></strong><br>
             </nav>
         </header>
-
-        <article>
-            <section>
-                @yield('content')
-            </section>
-        </article>
-
-        <footer>
-             <small><center>
-             <a href="https://www.rimji.com/about/">About</a> &nbsp &nbsp <a href="https://www.rimji.com/contact/">Contact</a> &nbsp &nbsp <a href="https://www.instagram.com/rimji.official/">Instagram</a><br>
-             &copy; 2021
-             </center></small>
-        </footer>
     </section>
 
     <script src="{{ mix('js/main.js', 'assets/build') }}"></script>
