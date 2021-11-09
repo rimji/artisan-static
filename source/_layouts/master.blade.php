9626446ad4e19740690762b26e3da882a5bb734a
rimji <88181976+rimji@users.noreply.github.com>
2021-11-09 19:27:22 +0530

    Update master.blade.php

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -21,9 +21,6 @@
         <header>
             <nav>
             <a href="https://www.rimji.com/"><img src="https://user-images.githubusercontent.com/88181976/129444878-06aa2a2a-b03c-4c54-b544-60cefb073e90.png" alt="rimji" width="120" height="60"></a>
-             <small><center>
-             Create Imagine  Play
-             </center></small>
             <br>
             </nav>
         </header>
