192cd9dfd240a0975bca6031308ca53d880adfd2
rimji <88181976+rimji@users.noreply.github.com>
2021-12-14 19:27:42 +0530

    Update master.blade.php

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -32,7 +32,7 @@
 
         <footer>
              <small><center>
-             <a href="https://www.rimji.com/about/">About</a> &nbsp &nbsp <br><a href="https://www.rimji.com/contact/">Contact</a> &nbsp &nbsp<br> <a href="https://www.instagram.com/houseofrimji/">Instagram</a>&nbsp &nbsp<br><a href="https://www.facebook.com/houseofrimji/">Facebook</a><br>
+             <a href="https://www.rimji.com/about/">About</a><br><a href="https://www.rimji.com/contact/">Contact</a><br><a href="https://www.instagram.com/houseofrimji/">Instagram</a><br><a href="https://www.facebook.com/houseofrimji/">Facebook</a><br>
              &copy; 2021 House of Rimji
              </center></small>
         </footer>
