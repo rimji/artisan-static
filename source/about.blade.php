4c9322be22b8c47b3b7b3130f761afda564f5d6b
rimji <88181976+rimji@users.noreply.github.com>
2021-12-14 16:36:28 +0530

    Update about.blade.php

--- source/about.blade.php
+++ source/about.blade.php
@@ -5,6 +5,6 @@
 @section('content')
    <small><center>
    THE HOUSE OF RIMJI<br>
-   The House of Rimji has been creating the most exclusive and high quality products.
+   We create the most exclusive and high quality products.
    </center> </small>
 @endsection
