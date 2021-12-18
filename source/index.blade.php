2b3396ab6f0d75d4349ac82d9d10431deec58023
rimji <88181976+rimji@users.noreply.github.com>
2021-12-18 17:09:01 +0530

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <center>
     <small>
-  
+  <a href="https://www.rimji.com/"><img src="https://user-images.githubusercontent.com/88181976/146639557-b8c19252-fd8e-4a1d-8b48-7d09d67d35f9.png" alt="Rimji" width="60" height="60"></a>
     </small>
     </center>
 @endsection
