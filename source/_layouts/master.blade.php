342e1211e7359022a5557b2343897290569622c9
rimji <88181976+rimji@users.noreply.github.com>
2021-12-30 20:31:31 +0530

    Update master.blade.php

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -32,10 +32,10 @@
 
         <footer>
          <small><center>
-             <a href="https://www.facebook.com/houseofrimji"><img src="https://user-images.githubusercontent.com/88181976/146122205-1ad12db6-cca7-41b8-a420-99d7f69d762e.png" alt="Facebook" width="27" height="27"></a>
-             &nbsp
-             <a href="https://www.instagram.com/houseofrimji"><img src="https://user-images.githubusercontent.com/88181976/146123677-ffcd5aa5-c7d4-4555-96b7-9f23006b0697.png" alt="Instagram" width="23" height="23"></a>
-             <br>
+             <a href="https://www.facebook.com/houseofrimji">Facebook</a>
+             &nbsp&nbsp
+             <a href="https://www.instagram.com/houseofrimji">Instagram</a>
+             &nbsp&nbsp
              <a href="https://www.rimji.com/contact/">Contact Us</a>
              <br>
              &copy; 2021 House of Rimji
