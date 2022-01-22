d9eaeca7be0986e1860ae6d8650696ce1c35bd9f
rimji <88181976+rimji@users.noreply.github.com>
2022-01-22 17:21:55 +0530

    Update beauty.blade.php

--- source/beauty.blade.php
+++ source/beauty.blade.php
@@ -5,11 +5,6 @@
 @section('content')
    <small><center>
    @rimjibeauty<br>
-   Handmade with Love<br><br><br>        
-   <a href="https://www.facebook.com/rimjibeauty">facebook</a>
-   &nbsp&nbsp
-   <a href="https://www.instagram.com/rimjibeauty">instagram</a>
-   &nbsp&nbsp
-   <a href="https://www.rimji.com/contactbeauty/">contact us</a>
+   Handmade with Love<br>
    </center> </small>
 @endsection
