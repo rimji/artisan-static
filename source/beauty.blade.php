2cfaa52460cacb5568ea10c9d52f501abff647da
rimji <88181976+rimji@users.noreply.github.com>
2022-01-23 18:50:55 +0530

    Update beauty.blade.php

--- source/beauty.blade.php
+++ source/beauty.blade.php
@@ -6,6 +6,6 @@
    <small><center>
    Rimji Beauty Products<br>
    <u>Face Oil</u>. <u>Body Oil</u>. <u>Hair Oil</u><br>
-   All Natural.<br>Fragrance Free.<br>Preservative Free
+   All Natural.<br>Fragrance Free.<br>Preservative Free.
    </center> </small>
 @endsection
