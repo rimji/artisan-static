47076b6369a9d3d25f32edb004d3daeb644a3216
rimji <88181976+rimji@users.noreply.github.com>
2022-01-23 18:37:04 +0530

    Create faceoil.blade.php

---
+++ source/faceoil.blade.php
@@ -0,0 +1,11 @@
+@extends('_layouts.master')
+
+@section('title', 'Face Oil')
+
+@section('content')
+   <small><center>
+   Rimji Face Oil<br>
+   Natural. Fresh. Handmade<br>
+   Fragrance Free. Presevative Free
+   </center> </small>
+@endsection
