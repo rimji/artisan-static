a40fd2cb208139ced14b63ce220baf6e9177f656
rimji <88181976+rimji@users.noreply.github.com>
2022-01-23 19:45:13 +0530

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <small><center>
-    <a href="https://www.rimji.com/faceoil/"><u>Face Oil</u></a>&nbsp&nbsp<a href="https://www.rimji.com/bodyoil/"><u>Body Oil</u></a>&nbsp&nbsp<a href="https://www.rimji.com/hairoil/"><u>Hair Oil</u></a><br><br>
+    <a href="https://www.rimji.com/faceoil/"><u>Face Oil</u></a>&nbsp|&nbsp<a href="https://www.rimji.com/bodyoil/"><u>Body Oil</u></a>&nbsp|&nbsp<a href="https://www.rimji.com/hairoil/"><u>Hair Oil</u></a><br><br>
     All Natural.<br>Fragrance Free.<br>Preservative Free.
     </center> </small>
 @endsection
