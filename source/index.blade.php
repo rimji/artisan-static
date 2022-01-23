ec0278b58a173a7232f3743e23fdaf0d65ecdd36
rimji <88181976+rimji@users.noreply.github.com>
2022-01-23 19:23:15 +0530

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <center>
     <small>
-    <a href="https://www.rimji.com/beauty/">@rimjibeauty</a>
+    <a href="https://www.rimji.com/beauty/">@<u>rimjibeauty</u></a>
     </small>
     </center>
 @endsection
