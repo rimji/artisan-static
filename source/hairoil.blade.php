79458367cddc6c0b904681afeacd91dc8c6a2ea3
rimji <88181976+rimji@users.noreply.github.com>
2022-01-23 18:40:18 +0530

    Create hairoil.blade.php

---
+++ source/hairoil.blade.php
@@ -0,0 +1,11 @@
+@extends('_layouts.master')
+
+@section('title', 'Hair Oil')
+
+@section('content')
+   <small><center>
+   Rimji Hair Oil<br>
+   Natural. Fresh. Handmade<br>
+   Fragrance Free. Presevative Free
+   </center> </small>
+@endsection
