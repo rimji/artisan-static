922b425badba8c29d7d42f4f43d4234c581bcdbd
rimji <88181976+rimji@users.noreply.github.com>
2022-01-23 18:38:46 +0530

    Create bodyoil.blade.php

---
+++ source/bodyoil.blade.php
@@ -0,0 +1,11 @@
+@extends('_layouts.master')
+
+@section('title', 'Body Oil')
+
+@section('content')
+   <small><center>
+   Rimji Body Oil<br>
+   Natural. Fresh. Handmade<br>
+   Fragrance Free. Presevative Free
+   </center> </small>
+@endsection
