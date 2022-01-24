7300d8a1467fa4ba12ae74e0ce6afaa0625f23e1
rimji <88181976+rimji@users.noreply.github.com>
2022-01-24 13:41:41 +0530

    Update beauty.blade.php

--- source/beauty.blade.php
+++ source/beauty.blade.php
@@ -7,9 +7,10 @@
     <a href="https://www.rimji.com/faceoil/"><u>Face Oil</u></a>&nbsp&nbsp<a href="https://www.rimji.com/bodyoil/"><u>Body Oil</u></a>&nbsp&nbsp<a href="https://www.rimji.com/hairoil/"><u>Hair Oil</u></a><br><br>
     All Natural.<br>Fragrance Free.<br>Preservative Free.<br><br>
     
-<p align="justify">
-    Note: Our oils are preservative free. Store them away from moisture or any kind of water contamination. Secure the cap tightly after each use. Use these oils within 6 months from opening. 
-</p>
+   <p align="justify">
+    Note: Our oils are preservative free. Store them away from moisture or any kind of water contamination. Secure the cap tightly after each use. Use these oils within 6 months from opening.<br>
+    Though most people don’t have issues using these oils, people with allergies could have a potential reaction. If you think your skin will react, we recommend performing a 24-hour patch test on your wrist and neck first and slowly integrating them into your routine thereafter. If you do experience an allergic reaction, stop using the product immediately and seek medical attention if needed.
+   </p>
 @rimjibeauty
 </center> </small>
 @endsection
