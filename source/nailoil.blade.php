ad0cdfae6afff73151922538e269d383ef34f93b
rimji <88181976+rimji@users.noreply.github.com>
2022-01-24 20:31:10 +0530

    Create nailoil.blade.php

---
+++ source/nailoil.blade.php
@@ -0,0 +1,11 @@
+@extends('_layouts.master')
+
+@section('title', 'Nail Oil')
+
+@section('content')
+   <small><center>
+   Nail Oil<br><br>
+   All Natural.<br>Fragrance Free.<br>Preservative Free.<br><br>
+   <a href="https://www.rimji.com/beauty/"><u>Care Instructions</u>
+   </center> </small>
+@endsection
