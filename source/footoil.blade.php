47fc464623abda298998e3f098bfe1acaca4dce5
rimji <88181976+rimji@users.noreply.github.com>
2022-01-24 20:27:38 +0530

    Create footoil.blade.php

---
+++ source/footoil.blade.php
@@ -0,0 +1,11 @@
+@extends('_layouts.master')
+
+@section('title', 'Foot Oil')
+
+@section('content')
+   <small><center>
+   Foot Oil<br><br>
+   All Natural.<br>Fragrance Free.<br>Preservative Free.<br><br>
+   <a href="https://www.rimji.com/beauty/"><u>Care Instructions</u>
+   </center> </small>
+@endsection
