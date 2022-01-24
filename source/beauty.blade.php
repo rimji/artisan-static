3a7e5d05ad3d38fcbb830cc0d9fb1f91e65ff924
rimji <88181976+rimji@users.noreply.github.com>
2022-01-24 13:19:09 +0530

    Update beauty.blade.php

--- source/beauty.blade.php
+++ source/beauty.blade.php
@@ -7,9 +7,6 @@
     <a href="https://www.rimji.com/faceoil/"><u>Face Oil</u></a>&nbsp&nbsp<a href="https://www.rimji.com/bodyoil/"><u>Body Oil</u></a>&nbsp&nbsp<a href="https://www.rimji.com/hairoil/"><u>Hair Oil</u></a><br><br>
     All Natural.<br>Fragrance Free.<br>Preservative Free.<br><br>
     @rimjibeauty<br><br>
-    Note<br>
-    Store these oils away from moisture or any kind of water contamination.<br>
-    Secure the cap tightly after each use.<br>
-    Use these oils within 6 months from opening.
+    Note: Store these oils away from moisture or any kind of water contamination. Secure the cap tightly after each use. Use these oils within 6 months from opening.
    </center> </small>
 @endsection
