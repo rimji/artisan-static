7c7c10d7c4803892d06a88fbe89608f80cd2344d
rimji <88181976+rimji@users.noreply.github.com>
2022-01-24 13:44:16 +0530

    Update beauty.blade.php

--- source/beauty.blade.php
+++ source/beauty.blade.php
@@ -8,8 +8,8 @@
     All Natural.<br>Fragrance Free.<br>Preservative Free.<br><br>
     
    <p align="justify">
-    Note: Our oils are preservative free. Store them away from moisture or any kind of water contamination. Secure the cap tightly after each use. Use these oils within 6 months from opening.<br>
-    Though most people don’t have issues using these oils, people with allergies could have a potential reaction. If you think your skin will react, we recommend performing a 24-hour patch test on your wrist and neck first and slowly integrating them into your routine thereafter. If you do experience an allergic reaction, stop using the product immediately and seek medical attention if needed.
+    Note: Our oils are preservative free. Store them away from moisture or any kind of water contamination. Secure the cap tightly after each use. Use these oils within 6 months from opening.<br><br>
+    Patch test: Though most people don’t have issues using these oils, people with allergies could have a potential reaction. If you think your skin will react, we recommend performing a 24-hour patch test on your wrist and neck first and slowly integrating them into your routine thereafter. If you do experience an allergic reaction, stop using the product immediately and seek medical attention if needed.
    </p>
 @rimjibeauty
 </center> </small>
