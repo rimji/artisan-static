9f96bc91d72539e13cd7c7c3c119636e25a95019
rimji <88181976+rimji@users.noreply.github.com>
2022-01-24 14:17:06 +0530

    Update beauty.blade.php

--- source/beauty.blade.php
+++ source/beauty.blade.php
@@ -6,7 +6,7 @@
    <small><center>
     @rimjibeauty<br><br>
     <a href="https://www.rimji.com/faceoil/"><u>Face Oil</u></a>&nbsp&nbsp<a href="https://www.rimji.com/bodyoil/"><u>Body Oil</u></a>&nbsp&nbsp<a href="https://www.rimji.com/hairoil/"><u>Hair Oil</u></a><br><br>
-    All Natural.<br>Fragrance Free.<br>Preservative Free.<br>Colourant Free.<br><br>
+    All Natural.<br>Fragrance Free.<br>Preservative Free.<br><br>
    <p align="justify">
     These oils are suitable for everyday use and are recommended for all skin types.<br><br> 
     Note: Our oils are preservative free. Store them away from moisture or any kind of water contamination. Secure the cap tightly after each use. Use these oils within 6 months from opening.<br><br>
