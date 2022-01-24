3b16370a353de12464bddac1fa7b9574ab5f8c07
rimji <88181976+rimji@users.noreply.github.com>
2022-01-24 19:25:19 +0530

    Update faceoil.blade.php

--- source/faceoil.blade.php
+++ source/faceoil.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
    <small><center>
-   Face Oil<br><br>
+   <mark>Face Oil</mark><br><br>
    All Natural.<br>Fragrance Free.<br>Preservative Free.<br><br>
    <a href="https://www.rimji.com/beauty/"><u>Care Instructions</u>
    </center> </small>
