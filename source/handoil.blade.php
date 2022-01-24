d17577b464dc9a3476c39a09fa35818cd142f6fd
rimji <88181976+rimji@users.noreply.github.com>
2022-01-24 20:30:23 +0530

    Create handoil.blade.php

---
+++ source/handoil.blade.php
@@ -0,0 +1,11 @@
+@extends('_layouts.master')
+
+@section('title', 'Hand Oil')
+
+@section('content')
+   <small><center>
+   Hand Oil<br><br>
+   All Natural.<br>Fragrance Free.<br>Preservative Free.<br><br>
+   <a href="https://www.rimji.com/beauty/"><u>Care Instructions</u>
+   </center> </small>
+@endsection
