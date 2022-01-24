ccf4eecc7fd2f0032fe10d23a5e3f277a068449c
rimji <88181976+rimji@users.noreply.github.com>
2022-01-24 16:28:19 +0530

    Update hairoil.blade.php

--- source/hairoil.blade.php
+++ source/hairoil.blade.php
@@ -5,6 +5,7 @@
 @section('content')
    <small><center>
    Hair Oil<br><br>
-   All Natural.<br>Fragrance Free.<br>Preservative Free.
+   All Natural.<br>Fragrance Free.<br>Preservative Free.<br><br>
+   <a href="https://www.rimji.com/beauty/"><u>Care Instructions</u>
    </center> </small>
 @endsection
