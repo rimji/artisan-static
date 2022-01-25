2b4de24855eb801b375928a613f90eb16def311e
rimji <88181976+rimji@users.noreply.github.com>
2022-01-25 12:30:55 +0530

    Update hocare.blade.php

--- source/hocare.blade.php
+++ source/hocare.blade.php
@@ -8,7 +8,7 @@
   <p align="justify">
     This oil is suitable for everyday use and is recommended for all hair types.<br><br> 
     Note: This oil is preservative free. Store it away from moisture or any kind of water contamination. Secure the cap tightly after each use. Use this oil within 6 months from opening.<br><br>
-    Patch test: Though most people don’t have issues using this oil, people with allergies could have a potential reaction. If you think your skin will react, we recommend performing a 24-hour patch test on your hairline first and slowly integrating them into your routine thereafter. If you do experience an allergic reaction, stop using the product immediately and seek medical attention if needed.<br><br>
+    Patch test: Though most people don’t have issues using this oil, people with allergies could have a potential reaction. If you think your skin will react, we recommend performing a 24-hour patch test on your hairline first and slowly integrating it into your routine thereafter. If you do experience an allergic reaction, stop using the product immediately and seek medical attention if needed.<br><br>
     Transition:  If you have switched to an all-natural/organic product for the first time, your scalp may go under a transition before you start observing results. You may also encounter hair fall during this adjustment phase. Losing 50 to 100 strands a day is absolutely normal as new hair tends to replace the lost ones. However, if the number of strands you lose every day increases considerably we would recommend consulting a dermatologist to ascertain the cause and take remedial actions.
   </p>
    </center> </small>
