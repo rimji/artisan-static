e8d127b80b3e271e9ff061c49eb6dad28d47d812
rimji <88181976+rimji@users.noreply.github.com>
2022-01-25 19:07:59 +0530

    Update hairoil.blade.php

--- source/hairoil.blade.php
+++ source/hairoil.blade.php
@@ -5,7 +5,9 @@
 @section('content')
    <small><center>
    Hair Oil<br><br>
-   All Natural.<br>Fragrance Free.<br>Preservative Free.<br><br>
+   <p align="justify">
+    Natural, Fresh and Handmade. Free from Fragrance, Colourants and Preservatives. Not tested on animals. 100% Vegetarian.
+   </p>
    <a href="https://www.rimji.com/hocare/"><u>Care Instructions</u>
    </center> </small>
 @endsection
