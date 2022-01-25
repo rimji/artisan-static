7c6fc26d347915fe31d1c7b13f95e6f3156ae21d
rimji <88181976+rimji@users.noreply.github.com>
2022-01-25 19:52:45 +0530

    Update bodyoil.blade.php

--- source/bodyoil.blade.php
+++ source/bodyoil.blade.php
@@ -9,6 +9,6 @@
     Natural, Fresh and Handmade. Free from Fragrance, Colourants and Preservatives. Not tested on animals. 100% Vegetarian.
    </p>
    <a href="https://www.rimji.com/bocare/"><u>Care Instructions</u></a><br><br>
-   Liked Body Oil?<br>Try <a href="https://www.rimji.com/hairoil/"><u>Hair Oil</u></a> also.
+   Liked Body Oil?<br>Try <a href="https://www.rimji.com/hairoil/"><u>Hair Oil</u></a>.
    </center> </small>
 @endsection
