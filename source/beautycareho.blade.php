33dd2fd3497d7ae1da9c8cb488e230eabfe79dbc
rimji <88181976+rimji@users.noreply.github.com>
2022-01-25 12:22:36 +0530

    Update beautycareho.blade.php

--- source/beautycareho.blade.php
+++ source/beautycareho.blade.php
@@ -4,10 +4,10 @@
 
 @section('content')
    <small><center>
-    Care Instructions<br><br>
+    Hair Oil Care Instructions<br><br>
   <p align="justify">
-    These oils are suitable for everyday use and are recommended for all skin types.<br><br> 
-    Note: Our oils are preservative free. Store them away from moisture or any kind of water contamination. Secure the cap tightly after each use. Use these oils within 6 months from opening.<br><br>
+    This oil is suitable for everyday use and is recommended for all hair types.<br><br> 
+    Note: This oil is preservative free. Store it away from moisture or any kind of water contamination. Secure the cap tightly after each use. Use this oil within 6 months from opening.<br><br>
     Patch test: Though most people don’t have issues using this oil, people with allergies could have a potential reaction. If you think your skin will react, we recommend performing a 24-hour patch test on your hairline first and slowly integrating them into your routine thereafter. If you do experience an allergic reaction, stop using the product immediately and seek medical attention if needed.<br><br>
     Transition:  If you have switched to an all-natural/organic product for the first time, your scalp may go under a transition before you start observing results. You may also encounter hair fall during this adjustment phase. Losing 50 to 100 strands a day is absolutely normal as new hair tends to replace the lost ones. However, if the number of strands you lose every day increases considerably we would recommend consulting a dermatologist to ascertain the cause and take remedial actions.
   </p>
