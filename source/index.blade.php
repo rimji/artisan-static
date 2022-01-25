23d0445a5bbe0e1728a6f7b3bc1102dfc60097c5
rimji <88181976+rimji@users.noreply.github.com>
2022-01-25 11:53:28 +0530

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -2,11 +2,7 @@
 
 @section('content')
     <small><center>
-    Oils for 
-    <a href="https://www.rimji.com/faceoil/"><u>Face</u></a>,&nbsp&nbsp<a href="https://www.rimji.com/bodyoil/"><u>Body</u></a>,&nbsp&nbsp<a href="https://www.rimji.com/hairoil/"><u>Hair</u></a>,
-    <a href="https://www.rimji.com/lipoil/"><u>Lips</u></a>,&nbsp&nbsp<a href="https://www.rimji.com/footoil/"><u>Feet</u></a>,&nbsp&nbsp<a href="https://www.rimji.com/nailoil/"><u>Nails</u></a> and
-    <a href="https://www.rimji.com/handoil/"><u>Hands</u></a>.<br><br>
-   
+    <a href="https://www.rimji.com/bodyoil/"><u>Body Oil</u></a>&nbsp&nbsp<a href="https://www.rimji.com/hairoil/"><u>Hair Oil</u></a><br><br>
     All Natural.<br>Fragrance Free.<br>Preservative Free.
     </center> </small>
 @endsection
