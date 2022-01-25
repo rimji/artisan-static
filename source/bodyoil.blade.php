afb9c9ef0737929c684c3f6dfdd75de7d9939c58
rimji <88181976+rimji@users.noreply.github.com>
2022-01-25 17:25:22 +0530

    Update bodyoil.blade.php

--- source/bodyoil.blade.php
+++ source/bodyoil.blade.php
@@ -5,7 +5,7 @@
 @section('content')
    <small><center>
    Body Oil<br><br>
-   All Natural.<br>Fragrance Free.<br>Preservative Free.<br><br>
+   All Natural.<br>Fragrance Free.<br>Colourant Free.<br>Preservative Free.<br>100% Vegetarian.<br>Cruelty Free.<br><br>
    <a href="https://www.rimji.com/bocare/"><u>Care Instructions</u>
    </center> </small>
 @endsection
