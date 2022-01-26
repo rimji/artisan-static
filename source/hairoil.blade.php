93db507f4d09567f5eec020c6009f86a90a11256
rimji <88181976+rimji@users.noreply.github.com>
2022-01-26 15:58:01 +0530

    Update hairoil.blade.php

--- source/hairoil.blade.php
+++ source/hairoil.blade.php
@@ -6,7 +6,8 @@
    <small><center>
    Hair Oil<br><br>
    <p align="justify">
-    Natural, Fresh and Handmade. Free from Fragrance, Colourants and Preservatives. Not tested on animals. 100% Vegetarian.
+    Natural, Fresh and Handmade. Free from Fragrance, Colourants and Preservatives. Not tested on animals. 100% Vegetarian.<br><br>
+    Usage: 1. Gently massage the oil into the scalp. 2. Coat the lengths of your hair with the oil, concentrating on the ends. 3. Leave it on for at least an hour, or overnight for better results.
    </p>
    <a href="https://www.rimji.com/hocare/"><u>Care Instructions</u></a><br><br>
    Liked Hair Oil?<br>Try <a href="https://www.rimji.com/bodyoil/"><u>Body Oil</u></a>
