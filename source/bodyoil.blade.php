9e6a39842e319f16e7a8c697a0398c0310e5fda2
rimji <88181976+rimji@users.noreply.github.com>
2022-01-26 15:37:07 +0530

    Update bodyoil.blade.php

--- source/bodyoil.blade.php
+++ source/bodyoil.blade.php
@@ -6,8 +6,9 @@
    <small><center>
    Body Oil<br><br>
    <p align="justify">
-    Natural, Fresh and Handmade. Free from Fragrance, Colourants and Preservatives. Not tested on animals. 100% Vegetarian.
-   </p>
+    Natural, Fresh and Handmade. Free from Fragrance, Colourants and Preservatives. Not tested on animals. 100% Vegetarian.<br><br>
+    Usage: 1. Take a little amount of body oil and spread it on clean palms. 2. Apply on the body and gently massage in circular motions until fully absorbed. 3. Apply an additional layer to extra dry patches such as the surface of elbows and knees.
+ </p>
    <a href="https://www.rimji.com/bocare/"><u>Care Instructions</u></a><br><br>
    Liked Body Oil?<br>Try <a href="https://www.rimji.com/hairoil/"><u>Hair Oil</u></a>
    </center> </small>
