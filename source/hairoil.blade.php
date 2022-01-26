790994ae1d540f5e48931ec718d72ed7421c2372
rimji <88181976+rimji@users.noreply.github.com>
2022-01-26 18:12:29 +0530

    Update hairoil.blade.php

--- source/hairoil.blade.php
+++ source/hairoil.blade.php
@@ -8,9 +8,8 @@
    <p align="justify">
     Use this nourishing hair oil for healthier hair and scalp.<br><br>
     Natural, Fresh and Handmade. Free from Fragrance, Colourants and Preservatives. Not tested on animals. 100% Vegetarian.<br><br>
-    Usage: 1. Gently massage the oil into the scalp. 2. Coat the lengths of your hair with the oil, concentrating on the ends. 3. Leave it on for at least an hour, or overnight for better results.
-   </p>
-   <a href="https://www.rimji.com/hocare/"><u>Care Instructions</u></a><br><br>
-   Liked Hair Oil?<br>Try <a href="https://www.rimji.com/bodyoil/"><u>Body Oil</u></a>
+    Usage: 1. Gently massage the oil into the scalp. 2. Coat the lengths of your hair with the oil, concentrating on the ends. 3. Leave it on for at least an hour, or overnight for better results. <a href="https://www.rimji.com/hocare/"><u>Care Instructions</u></a>
+  </p>
+    Liked Hair Oil?<br>Try <a href="https://www.rimji.com/bodyoil/"><u>Body Oil</u></a>
    </center> </small>
 @endsection
