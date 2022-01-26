ab4a2c27632831f5797680f4838b4262c89befd1
rimji <88181976+rimji@users.noreply.github.com>
2022-01-26 19:33:11 +0530

    Update bodyoil.blade.php

--- source/bodyoil.blade.php
+++ source/bodyoil.blade.php
@@ -11,6 +11,6 @@
     Ingredients: Sesamum Indicum Oil, Cocos Nucifera Oil, Prunus Amygdalus Dulcis Oil.<br><br>
     Usage: 1. Take a little amount of oil and spread it on clean palms. 2. Apply on the body and gently massage in circular motions until fully absorbed. 3. Apply an additional layer to extra dry patches such as the surface of elbows and knees. <a href="https://www.rimji.com/bocare/"><u>Care Instructions</u></a>
    </p>
-    Liked Body Oil?<br>Try <a href="https://www.rimji.com/hairoil/"><u>Hair Oil</u></a>
+    Like Body Oil?<br>Try <a href="https://www.rimji.com/hairoil/"><u>Hair Oil</u></a>
    </center> </small>
 @endsection
