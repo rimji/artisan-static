b98c1393f960fce346ac966bb884bc3f3c4c189f
rimji <88181976+rimji@users.noreply.github.com>
2022-01-26 19:34:34 +0530

    Update hairoil.blade.php

--- source/hairoil.blade.php
+++ source/hairoil.blade.php
@@ -11,6 +11,6 @@
     Ingredients: Sesamum Indicum Oil, Cocos Nucifera Oil, Ricinus Communis Oil.<br><br>
     Usage: 1. Gently massage the oil into the scalp. 2. Coat the lengths of your hair with the oil, concentrating on the ends. 3. Leave it on for at least an hour, or overnight for better results. <a href="https://www.rimji.com/hocare/"><u>Care Instructions</u></a>
   </p>
-    Liked Hair Oil?<br>Try <a href="https://www.rimji.com/bodyoil/"><u>Body Oil</u></a>
+    Like Hair Oil?<br>Try <a href="https://www.rimji.com/bodyoil/"><u>Body Oil</u></a>
    </center> </small>
 @endsection
