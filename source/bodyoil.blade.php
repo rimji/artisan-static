180d33abd251c0480a46edd864e542c665c251b5
rimji <88181976+rimji@users.noreply.github.com>
2022-01-26 19:23:16 +0530

    Update bodyoil.blade.php

--- source/bodyoil.blade.php
+++ source/bodyoil.blade.php
@@ -8,7 +8,7 @@
    <p align="justify">
     Use this nourishing oil moisturizer for soft, healthy and smooth skin.<br><br>
     Natural, Fresh and Handmade. Free from Fragrance, Colourants and Preservatives. Not tested on animals. 100% Vegetarian.<br><br>
-    Ingredients: Sesamum indicum Oil, Cocos nucifera Oil, Prunus Amygdalus Dulcis Oil.<br><br>
+    Ingredients: Sesamum Indicum Oil, Cocos Nucifera Oil, Prunus Amygdalus Dulcis Oil.<br><br>
     Usage: 1. Take a little amount of oil and spread it on clean palms. 2. Apply on the body and gently massage in circular motions until fully absorbed. 3. Apply an additional layer to extra dry patches such as the surface of elbows and knees. <a href="https://www.rimji.com/bocare/"><u>Care Instructions</u></a>
    </p>
     Liked Body Oil?<br>Try <a href="https://www.rimji.com/hairoil/"><u>Hair Oil</u></a>
