697fc6de0989dd442d6a576d70bcd0d2bae56d8c
rimji <88181976+rimji@users.noreply.github.com>
2022-01-26 18:09:18 +0530

    Update bodyoil.blade.php

--- source/bodyoil.blade.php
+++ source/bodyoil.blade.php
@@ -8,9 +8,8 @@
    <p align="justify">
     Use this nourishing oil moisturizer for soft, healthy and smooth skin.<br><br>
     Natural, Fresh and Handmade. Free from Fragrance, Colourants and Preservatives. Not tested on animals. 100% Vegetarian.<br><br>
-    Usage: 1. Take a little amount of oil and spread it on clean palms. 2. Apply on the body and gently massage in circular motions until fully absorbed. 3. Apply an additional layer to extra dry patches such as the surface of elbows and knees.
- </p>
-   <a href="https://www.rimji.com/bocare/"><u>Care Instructions</u></a><br><br>
-   Liked Body Oil?<br>Try <a href="https://www.rimji.com/hairoil/"><u>Hair Oil</u></a>
+    Usage: 1. Take a little amount of oil and spread it on clean palms. 2. Apply on the body and gently massage in circular motions until fully absorbed. 3. Apply an additional layer to extra dry patches such as the surface of elbows and knees. <a href="https://www.rimji.com/bocare/"><u>Care Instructions</u></a>
+</p>
+    Liked Body Oil?<br>Try <a href="https://www.rimji.com/hairoil/"><u>Hair Oil</u></a>
    </center> </small>
 @endsection
