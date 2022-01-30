91cba4ec9977f6310ace47402b24433fc36439ca
rimji <88181976+rimji@users.noreply.github.com>
2022-01-30 12:21:53 +0530

    Update master.blade.php

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -20,7 +20,7 @@
     <section>
         <header>
             <nav> 
-            <a href="https://www.rimji.com/">R I M J I  B E A U T Y</a>
+            <a href="https://www.rimji.com/">R I M J I &nbsp B E A U T Y</a>
             </nav>
         </header>
 
