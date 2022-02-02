120f86578eb669cdc8908da0ec8a81cb6125e410
rimji <88181976+rimji@users.noreply.github.com>
2022-02-02 16:31:27 +0530

    Update bodyoil.blade.php

--- source/bodyoil.blade.php
+++ source/bodyoil.blade.php
@@ -7,7 +7,7 @@
    Body Oil<br><br>
    <p align="justify">
     Use this nourishing oil for soft, healthy and smooth skin.<br><br>
-    This oil is natural, fresh and handmade. It's fragrance, colourant and preservative free, not tested on animals and is 100% vegetarian.<br><br>
+    Handmade using natural and fresh ingredients. No fragrance, colour or preservative added. Not tested on animals and is 100% vegetarian.<br><br>
     Ingredients: Sesamum Indicum Oil, Prunus Amygdalus Dulcis Oil.<br><br>
     Usage: 1. Take a little amount of oil and spread it on clean palms. 2. Apply on the body and gently massage in circular motions until fully absorbed. 3. Apply an additional layer to extra dry patches such as the surface of elbows and knees. <a href="https://www.rimji.com/bocare/"><u>Care Instructions</u></a>
    </p>
