33658f0af14179804a5f46c088fd7c03a0f0cd28
rimji <88181976+rimji@users.noreply.github.com>
2022-02-02 12:33:53 +0530

    Update index.blade.php

--- source/index.blade.php
+++ source/index.blade.php
@@ -4,7 +4,7 @@
   <small><center>
     <a href="https://www.rimji.com/bodyoil/"><u>Body Oil</u></a><br><br><br>  
    <p align="justify">
-    Free from Fragrance, Colourants and Preservatives.
+    Natural, Fresh and Handmade. Free from Fragrance, Colourants and Preservatives. Not tested on animals. 100% Vegetarian.
    </p>
  </center> </small>
 @endsection
