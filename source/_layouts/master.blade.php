16107acdbdc12f204aade0be3f12b8bcdc0cc273
rimji <88181976+rimji@users.noreply.github.com>
2022-02-05 10:02:18 +0530

    Update master.blade.php

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -38,7 +38,7 @@
              &nbsp&nbsp
              <a href="https://www.rimji.com/contact/"><u>Contact us</u></a>
              <br>
-             &copy; RIMJI CO.
+             &copy; Rimji Co.
         </center></small>
         </footer>
     </section>
