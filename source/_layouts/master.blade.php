3c802e65ff95a92f301ae5753bf862afdcafbbae
rimji <88181976+rimji@users.noreply.github.com>
2022-02-07 10:20:56 +0530

    Update master.blade.php

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -20,7 +20,7 @@
     <section>
         <header>
             <nav> 
-            <a href="https://www.rimji.com"><b>Rimji<sup>&reg;</sup> Oil</b></a>
+            <a href="https://www.rimji.com"><b>Rimji<sup>&reg;</sup> Oil Co.</b></a>
             </nav>
         </header>
 
