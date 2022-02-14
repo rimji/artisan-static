ad7e49e111e0736cd589ce8b4cc29f3ee05e7a2e
rimji <88181976+rimji@users.noreply.github.com>
2022-02-14 17:37:03 +0530

    Update master.blade.php

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -20,7 +20,7 @@
     <section>
         <header>
             <nav> 
-             <a href="https://www.rimji.com/"><img src="https://user-images.githubusercontent.com/88181976/153827021-2c3c983e-c38d-4479-b18f-9aeec8a5a967.png" alt="Rimji Co." width="90" height="30"></a>
+             <a href="https://www.rimji.com/"><img src="https://user-images.githubusercontent.com/88181976/153827021-2c3c983e-c38d-4479-b18f-9aeec8a5a967.png" alt="Rimji Co." width="90" height="25"></a>
             </nav>
         </header>
 
