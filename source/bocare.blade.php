d656bfdd0fa8a9ee2bd48c2a1909d69058b48301
rimji <88181976+rimji@users.noreply.github.com>
2022-02-23 15:23:04 +0530

    Update bocare.blade.php

--- source/bocare.blade.php
+++ source/bocare.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Beauty')
 
 @section('content')
-   <center>
+   <center><small> 
        <a href="https://www.rimji.com/bodyoil/"><u>Body Oil</u></a> Care Instructions<br><br>
   <p align="justify">
     This oil is suitable for everyday use and is recommended for all skin types.<br><br> 
@@ -11,5 +11,5 @@
     Patch test: Though most people don’t have issues using this oil, people with allergies could have a potential reaction. If you think your skin will react, we recommend performing a 24-hour patch test on your wrist and neck first and slowly integrating it into your routine thereafter. If you do experience an allergic reaction, stop using the product immediately and seek medical attention if needed.<br><br>
     Detox/Skin Purge: If you have switched to an all-natural product for the first time, your skin may go under a detox before you start observing results. This detox period is commonly referred to as Skin Purge that results in a temporary acne flare-up. The skin purge period varies from individual to individual and can last up to 4 to 6 weeks. You can continue to use the product if it's skin purge and not an allergic reaction. Eventually, you'll notice the skin starts clearing out between the 4th to 6th week. We would recommend consulting a dermatologist to check if it’s a skin purge or a reaction.  
   </p>
-   </center>
+   </small></center>
 @endsection
